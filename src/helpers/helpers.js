0580f51150df0d2614d4142a8a30635c0b5289da
COMFAMILIAR <elmangones@hotmail.com>
2021-11-18 22:50:04 -0500

    Solucion Page Not Found Netlify

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,14 +1,3 @@
-import { stock } from "../data/stock"
-
-export const pedirProductos = () =>{
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve(stock)
-        }, 800)
-    })
-}
-
-
 export const currencyFormatter = (number) =>{
     return new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(number)
 }
